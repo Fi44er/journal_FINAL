a336ea746d2d3fd068191c3e58f7db3672c7cb1f
Fi44er <vaceslavmalugin81@gmail.com>
2024-02-08 11:17:29 +0500

    add cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -8,6 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe)
   app.use(cookieParser())
+  app.enableCors({origin: '*'})
 
   // ------------------ swagger --------------------- //
   const config = new DocumentBuilder()
